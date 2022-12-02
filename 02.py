1bcb4c821bd8dc9a1ff9290ca1ea66ddafb2b820
lousapetr <petr.lousa@datasentics.com>
2022-12-02 09:45:25 +0100

    solve day 2 part 1

--- 02.py
+++ 02.py
@@ -6,23 +6,56 @@ from wrapper import Wrapper
 class Solver(Wrapper):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.parser = self.parse_custom
+        self.parser = self.parse_to_list
         self.input = super().load_input()
 
-    def parse_custom(self, path):
-        with open(path) as f:
-            for line in f:
-                pass
+    @staticmethod
+    def remap_hands(orig_hands: list, hands_map: dict):
+        output = orig_hands.copy()
+        for orig, new in hands_map.items():
+            output = [hand.replace(orig, new) for hand in output]
+        return output
+
+    @staticmethod
+    def get_hand_score(hand: str):
+        """
+        match = '[ABC] [ABC]'
+            - remapped hands
+            - first is opponent, second myself
+
+        output = score
+            - A=1, B=2, C=3
+            - A>C, B>A, C>B
+                - 0 for lose, 3 for draw, 6 for win
+        """
+        hand = hand.replace(" ", "")
+
+        letter_score_map = {"A": 1, "B": 2, "C": 3}
+        letter_score = letter_score_map[hand[1]]
+
+        if hand[0] == hand[1]:
+            output_score = 3
+        elif hand in ("AB", "BC", "CA"):
+            output_score = 6
+        else:
+            output_score = 0
+
+        return letter_score + output_score
+
+    def get_total_score(self, hands: list):
+        return sum(self.get_hand_score(h) for h in hands)
 
     def task_1(self):
-        pass
+        hand_map = {"X": "A", "Y": "B", "Z": "C"}
+        mapped_hands = self.remap_hands(self.input, hand_map)
+        return self.get_total_score(mapped_hands)
 
     def task_2(self):
         pass
 
 
 part = 1
-solve_example = True
+solve_example = False
 example_solutions = [15, None]
 
 solver = Solver(day=2, example=solve_example, example_solutions=example_solutions)
