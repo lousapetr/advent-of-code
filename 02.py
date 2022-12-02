de6eb163e778f1540bc7aa4a7b9a8d6ef40c8c44
lousapetr <petr.lousa@datasentics.com>
2022-12-02 09:12:17 +0100

    start day 2

---
+++ 02.py
@@ -0,0 +1,33 @@
+from wrapper import Wrapper
+
+# https://adventofcode.com/2021/day/2
+
+
+class Solver(Wrapper):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.parser = self.parse_custom
+        self.input = super().load_input()
+
+    def parse_custom(self, path):
+        with open(path) as f:
+            for line in f:
+                pass
+
+    def task_1(self):
+        pass
+
+    def task_2(self):
+        pass
+
+
+part = 1
+solve_example = True
+example_solutions = [15, None]
+
+solver = Solver(day=2, example=solve_example, example_solutions=example_solutions)
+if solve_example:
+    solver.print_input()
+solver.solve_task(1)
+if part > 1:
+    solver.solve_task(2)
