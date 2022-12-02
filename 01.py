8fce54295e71af6363801b0b8539e3d4c3e091ba
lousapetr <petr.lousa@datasentics.com>
2022-12-02 09:01:50 +0100

    solve part 1

--- 01.py
+++ 01.py
@@ -4,10 +4,9 @@ from wrapper import Wrapper
 
 
 class Solver(Wrapper):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.parser = self.parse_custom
+        self.parser = self.parse_to_list
         self.input = super().load_input()
 
     def parse_custom(self, path):
@@ -16,15 +15,26 @@ class Solver(Wrapper):
                 pass
 
     def task_1(self):
-        pass
+        elf_calories = []
+        single_elf_cals = 0
+        for item in self.input:
+            if item:
+                single_elf_cals += int(item)
+            else:
+                elf_calories.append(single_elf_cals)
+                single_elf_cals = 0
+                continue
+        elf_calories.append(single_elf_cals)
+        print(elf_calories)
+        return max(elf_calories)
 
     def task_2(self):
         pass
 
 
 part = 1
-solve_example = True
-example_solutions = [MISSING, None]
+solve_example = False
+example_solutions = [24_000, None]
 
 solver = Solver(day=1, example=solve_example, example_solutions=example_solutions)
 if solve_example:
