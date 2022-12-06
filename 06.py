2988a89a9c42b8f3edf07e8da8e8f2ac26c7fb09
lousapetr <petr.lousa@datasentics.com>
2022-12-06 09:14:11 +0100

    solve day 6 part 2

--- 06.py
+++ 06.py
@@ -12,20 +12,23 @@ class Solver(Wrapper):
         self.parser = self.parse_to_list
         self.input = super().load_input()[0]
 
-    def task_1(self):
-        for i in range(4, len(self.input)):
-            substring = self.input[i - 4 : i]
+    def find_starter(self, distinct_count: int):
+        for i in range(distinct_count, len(self.input)):
+            substring = self.input[i - distinct_count : i]
             if len(substring) == len(set(substring)):
                 return i
 
+    def task_1(self):
+        return self.find_starter(4)
+
     def task_2(self):
-        return NotImplemented
+        return self.find_starter(14)
 
 
-part = 1
+part = 2
 solve_example = True
 solve_example = False
-example_solutions = [7, None]
+example_solutions = [7, 19]
 
 solver = Solver(
     day=DAY_NUMBER, example=solve_example, example_solutions=example_solutions
