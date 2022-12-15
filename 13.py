d1d2bdc44bd7695f6a0054b0b8a891cba274f314
lousapetr <petr.lousa@datasentics.com>
2022-12-15 11:05:59 +0100

    solve day 13 part 2

--- 13.py
+++ 13.py
@@ -1,6 +1,8 @@
 from wrapper import Wrapper
 from typing import List, Tuple, Set, Dict, Union  # noqa: F401
-from itertools import zip_longest
+from itertools import zip_longest, chain
+from functools import cmp_to_key
+from pprint import pprint
 
 # https://adventofcode.com/2022/day/13
 
@@ -25,22 +27,22 @@ class Solver(Wrapper):
                     pair = []
         return pairs
 
-    def compare(self, left: Union[List, int], right: Union[List, int]) -> str:
+    def compare(self, left: Union[List, int], right: Union[List, int]) -> int:
         """
-        Return which item is smaller - `left`, `right` or `tie`.
+        Return which item is smaller - `left` -> -1, `right` -> 1 or `tie` -> 0.
         """
         if left is None:  # left list was shorter
-            return 'left'
+            return -1
         if right is None:
-            return 'right'
+            return 1
 
         if type(left) == type(right) == int:
             if left < right:
-                return 'left'
+                return -1
             elif left == right:
-                return 'tie'
+                return 0
             else:
-                return 'right'
+                return 1
 
         if type(left) != type(right):
             if type(left) == int:
@@ -51,33 +53,39 @@ class Solver(Wrapper):
         # now both are lists
         for l, r in zip_longest(left, right):
             result = self.compare(l, r)
-            if result != 'tie':
+            if result != 0:
                 return result
         else:
-            return 'tie'
+            return 0
 
 
     def task_1(self):
         correct_order = []
         for i, (left, right) in enumerate(self.input):
-            print(f'{i:3d} / {len(self.input)}', end='\r')
             # print(f'{left=}')
             # print(f'{right=}')
             # print(f'{self.compare(left, right)=}')
             # print()
-            if self.compare(left, right) == 'left':
+            if self.compare(left, right) == -1:
                 correct_order.append(i + 1)
         print(f'{correct_order=}')
         return sum(correct_order)
 
     def task_2(self):
-        return NotImplemented
+        packets = list(chain.from_iterable(self.input))
+        dividers = [[[2]], [[6]]]
+        ordered = sorted(packets + dividers, key=cmp_to_key(self.compare))
+        pprint(ordered)
+        indices = [(d, ordered.index(d) + 1) for d in dividers]
+        print(f'{indices=}')
+        decoder_key = indices[0][1] * indices[1][1]
+        return decoder_key
 
 
-part = 1
+part = 2
 solve_example = True
 solve_example = False
-example_solutions = [13, None]
+example_solutions = [13, 140]
 
 solver = Solver(
     day=DAY_NUMBER, example=solve_example, example_solutions=example_solutions
