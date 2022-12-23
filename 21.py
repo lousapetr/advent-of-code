f82d75f9cd6cb2b6d0382c67f8393467ecda09d2
lousapetr <petr.lousa@datasentics.com>
2022-12-23 08:13:18 +0100

    solve day 21 part 2

--- 21.py
+++ 21.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import operator
-from typing import Callable, Dict, List, Set, Tuple, Union  # noqa: F401
+from typing import Any, Callable, Dict, List, Set, Tuple, Union  # noqa: F401
 
 from wrapper import Wrapper
 
@@ -18,6 +18,11 @@ class Monkey:
         self.name = name
         self.monkey_instances[name] = self
         self._assignment = assignment
+
+    @classmethod
+    def reset(cls):
+        for m in cls.monkey_instances.values():
+            m.set_children()
 
     def set_children(self):
         try:
@@ -36,16 +41,20 @@ class Monkey:
         elif op == "*":
             operation = operator.mul
         elif op == "/":
-            operation = operator.floordiv
+            operation = operator.truediv
         else:
             raise ValueError(f"Invalid operation {op}")
         return operation, children
 
     @property
-    def value(self) -> int:
+    def value(self) -> Any:
         if self._value is None:
             self._value = self.operation(*[m.value for m in self.children])
         return self._value
+
+    @value.setter
+    def value(self, value: Union[int, complex]):
+        self._value = value
 
 
 class Solver(Wrapper):
@@ -64,18 +73,26 @@ class Solver(Wrapper):
     def task_1(self):
         for monkey, assignment in self.input.items():
             Monkey(monkey, assignment)
-        for monkey in Monkey.monkey_instances.values():
-            monkey.set_children()
-        return Monkey.monkey_instances["root"].value
+        Monkey.reset()
+        return int(Monkey.monkey_instances["root"].value)
 
     def task_2(self):
-        return NotImplemented
+        for monkey, assignment in self.input.items():
+            Monkey(monkey, assignment)
+        Monkey.reset()
+
+        Monkey.monkey_instances["humn"].value = 0 + 1j  # use complex value as a polynom `0 + x`
+        Monkey.monkey_instances["root"].operation = lambda x, y: (x, y)  # evaluate both branches separately
+
+        # human side yields a polynom `real + imag * x`, monkey-only side yields number, solve equation for x
+        human, desired = sorted(Monkey.monkey_instances["root"].value, key=lambda x: str(type))
+        return int((desired - human.real) / human.imag)
 
 
-part = 1
+part = 2
 solve_example = True
 solve_example = False
-example_solutions = [152, None]
+example_solutions = [152, 301]
 
 solver = Solver(day=DAY_NUMBER, example=solve_example, example_solutions=example_solutions)
 if solve_example:
