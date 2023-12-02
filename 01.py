8a0e61e20e8609d449e3082dddbec64bc2782891
lousapetr <petr.lousa@datasentics.com>
2023-12-02 23:28:49 +0100

    solve day 1 pt 2

--- 01.py
+++ 01.py
@@ -1,6 +1,5 @@
-from typing import Dict, List, Set, Tuple  # noqa: F401
-
 from wrapper import Wrapper
+import re
 
 # --- Day 1: Trebuchet?! ---
 # https://adventofcode.com/2023/day/1
@@ -15,22 +14,40 @@ class Solver(Wrapper):
 
     def task_1(self):
         digits = ["".join(filter(lambda x: x.isdigit(), line)) for line in self.input]
-        numbers = [int(d[0] + d[-1]) for d in digits]
+        numbers = [self.first_last(d) for d in digits]
         return sum(numbers)
 
+    @staticmethod
+    def find_digits(string: str) -> list[str]:
+        number_names_raw = "one, two, three, four, five, six, seven, eight, nine"
+        number_names = number_names_raw.replace(",", "").split()
+        # https://stackoverflow.com/a/5616910
+        # finds all occurences, including overlaps
+        digits_raw =  re.findall(f"""(?=({"|".join(number_names) + "|[0-9]"}))""", string)
+        print(f"{digits_raw=}")
+        digits = [d if d.isdigit() else str(number_names.index(d) + 1) for d in digits_raw]
+        print(f"{digits=}")
+        return digits
+
+    @staticmethod
+    def first_last(digits: str) -> int:
+        return int(digits[0] + digits[-1])
+
     def task_2(self):
-        return NotImplemented
+        digits = [self.find_digits(s) for s in self.input]
+        numbers = [self.first_last(d) for d in digits]
+        return sum(numbers)
 
 
-part = 1
+part = 2
 
 solve_example = True
 solve_example = False
-example_solutions = [142, None]
+example_solutions = [142, 281]
 
 solver = Solver(day=DAY_NUMBER, example=solve_example, example_solutions=example_solutions)
 if solve_example:
     solver.print_input()
-solver.solve_task(1)
+# solver.solve_task(1)
 if part > 1:
-    solver.solve_task(2)
+    solver.solve_task(2, verbose=False)
