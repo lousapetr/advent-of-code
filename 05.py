7768c53705d8e09ac602610e90875cc90fb7cd4b
lousapetr <petr.lousa@datasentics.com>
2023-12-06 09:48:29 +0100

    start day 5

---
+++ 05.py
@@ -0,0 +1,40 @@
+"""
+DAILY_TITLE
+
+DAILY_URL
+"""
+
+import re
+
+from wrapper import Wrapper
+
+
+class Solver(Wrapper):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.parser = self.parse_custom
+
+    def parse_custom(self, path):
+        with open(path) as f:
+            blocks = f.read().split("\n\n")
+        seeds = [int(n) for n in re.findall(r"\d+", blocks[0])]
+        print(seeds)
+
+    def task_1(self):
+        return NotImplemented
+
+    def task_2(self):
+        return NotImplemented
+
+
+part = 1
+solve_example = True
+# solve_example = False
+example_solutions = [None, None]
+
+solver = Solver(day=5, example=solve_example, example_solutions=example_solutions)  # noqa: F821
+if solve_example:
+    solver.print_input()
+solver.solve_task(1)
+if part > 1:
+    solver.solve_task(2)
