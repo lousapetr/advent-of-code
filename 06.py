fc9d62576c0c8ce3f48413691d9569240fb354b6
lousapetr <petr.lousa@datasentics.com>
2023-12-06 11:53:14 +0100

    solve day 6 pt 2

--- 06.py
+++ 06.py
@@ -23,32 +23,37 @@ class Solver(Wrapper):
         distances = [int(n) for n in re.findall(r"\d+", distance_str)]
         return times, distances
 
+    def number_of_wins(self, time: int, dist: int) -> int:
+        """
+        the question is equivalent to
+        "how many integers solve equation `x*(time-x) - dist > 0`?"
+        equation is equivalent to `x**2 - time*x + dist < 0`
+        """
+        dist += 1e-6  # avoid the roots being exact integers
+        discriminant = time**2 - 4 * dist
+        root_low = (time - discriminant**0.5) / 2
+        root_high = (time + discriminant**0.5) / 2
+        wins = math.floor(root_high) - math.ceil(root_low) + 1
+        return wins
+
     def task_1(self):
         winning_ways = []
         for time, dist in zip(*self.input):
-            # print(f"{time=} {dist=}")
-            # the question is equivalent to
-            # how many integers solve equation `x*(time-x) - dist > 0`?
-            # equation is equivalent to `x**2 - time*x + dist < 0`
-            dist += 1e-6  # avoid the roots being exact integers
-            discriminant = time**2 - 4 * dist
-            # print(f"{discriminant=}")
-            # (7 +- 3.5) / 2
-            root_low = (time - discriminant**0.5) / 2
-            root_high = (time + discriminant**0.5) / 2
-            wins = math.floor(root_high) - math.ceil(root_low) + 1
-            winning_ways.append(wins)
+            winning_ways.append(self.number_of_wins(time, dist))
         print(f"{winning_ways=}")
         return math.prod(winning_ways)
 
     def task_2(self):
-        return NotImplemented
+        times, distances = self.input
+        time_str = "".join(str(n) for n in times)
+        dist_str = "".join(str(n) for n in distances)
+        return self.number_of_wins(int(time_str), int(dist_str))
 
 
-part = 1
+part = 2
 solve_example = True
 solve_example = False
-example_solutions = [288, None]
+example_solutions = [288, 71503]
 
 solver = Solver(day=6, example=solve_example, example_solutions=example_solutions)
 if solve_example:
