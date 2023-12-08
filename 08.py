c340b7ef11421e3fa236a123418d7dd8fe0f5ed6
lousapetr <petr.lousa@datasentics.com>
2023-12-08 13:53:22 +0100

    start day 8

---
+++ 08.py
@@ -0,0 +1,39 @@
+"""
+--- Day 8: Haunted Wasteland ---
+
+https://adventofcode.com/2023/day/8
+"""
+
+from pprint import pprint  # noqa: F401
+
+from wrapper import Wrapper
+
+
+class Solver(Wrapper):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.parser = self.parse_custom
+
+    def parse_custom(self, path):
+        with open(path) as f:
+            for line in f:
+                pass
+
+    def task_1(self):
+        return NotImplemented
+
+    def task_2(self):
+        return NotImplemented
+
+
+part = 1
+solve_example = True
+# solve_example = False
+example_solutions = [(2, 6), None]
+
+solver = Solver(day=8, example=solve_example, example_solutions=example_solutions)
+if solve_example:
+    solver.print_input()
+solver.solve_task(1)
+if part > 1:
+    solver.solve_task(2)
